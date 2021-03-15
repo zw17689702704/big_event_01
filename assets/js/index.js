3223e4c56ae89b46153c59027857b97e88799a54
zhuwei123 <zhuwei123@itcast.cn>
2021-03-15 09:59:06 +0800

    完成了主页功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,31 @@
 $(function () {
 
-
+    //需求1：ajax获取用户信息，渲染到页面
+    //这个功能，后面其他的页面\
     grtUserInfo();
+
+
+    //2.退出功能
+    let layer = layui.layer;
+    $("#btnLogout").on('click', function () {
+        layer.confirm('是否确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //1.清空本地token
+            localStorage.removeItem('myToken');
+            //2.页面跳转
+            location.href = '/login.html';
+            //关闭询问框  框架自带
+            layer.close(index);
+        });
+    })
 });
 
 function grtUserInfo() {
     $.ajax({
         url: '/my/userinfo',
         method: 'GET',
-        headers: {
-            Authorization: localStorage.getItem('myToken') || ''
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem('myToken') || ''
+        // },
         success: (res) => {
             // console.log(res);
             if (res.status != 0) {
@@ -18,7 +33,17 @@ function grtUserInfo() {
             }
             //头像和文字渲染
             renderAvatar(res.data);
-        }
+        },
+        // complete: function (res) {
+        //     console.log(res.responseJSON);
+        //     //判断如果状态是1 错误信息是身份认证失败  那么就销毁当前
+        //     let obj = res.responseJSON;
+        //     if (obj.status == 1 && obj.message === "身份认证失败！") {
+        //         //跳转到 登录页面 销毁token
+        //         localStorage.removeItem('myToken');
+        //         location.href = "/login.html";
+        //     }
+        // }
     });
 }
 
@@ -38,3 +63,4 @@ function renderAvatar(user) {
         $('.text-avatar').hide();
     }
 }
+
